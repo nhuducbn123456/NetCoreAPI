188393dcbc9b89522ec71b2eecc0c0293219fbaf
nhuducbn123456 <nhuducbn123456@gmail.com>
2023-10-05 14:18:33 +0700

    CreateProject

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NewApp.Models;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Person ps1 = new Person();
+        Person ps2 = new Person();
+        ps2.InputInfo();
+
+
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public void InputInfo()
+        {
+            Console.WriteLine("Nhap ma sinh vien: ");
+            Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap ten sinh vien: ");
+    
+        }
+    }
+}
